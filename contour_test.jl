cb62dbffff0434497df96e4d49b498c4638eff33
arturofburgos <burgos.arturo.ufu@gmail.com>
2024-03-18 18:59:20 -0500

    Contour and Heatmap test

---
+++ contour_test.jl
@@ -0,0 +1,24 @@
+using LaTeXStrings, Plots
+
+f(x, y) = (3x + y^2) * abs(sin(x) + cos(y))
+
+x = range(0, 5, length=100)
+y = range(0, 3, length=50)
+z = @. f(x', y)
+
+contour(x, y, z, levels=10, color=:viridis, clabels=true, cbar=false, lw=1)
+title!(L"Plot of $(3x + y^2)|\sin(x) + \cos(y)|$")
+xlabel!(L"x")
+ylabel!(L"y")
+
+contour(x, y, z, color=[:black])
+
+contour(x, y, z, fill=true)
+
+
+contourf(x, y, z, levels=20, color=:seaborn_rocket_gradient)
+title!(L"(3x + y^2)|\sin(x) + \cos(y)|")
+xlabel!(L"x")
+ylabel!(L"y")
+
+heatmap(x, y, z, levels=20, color=:seaborn_rocket_gradient)
